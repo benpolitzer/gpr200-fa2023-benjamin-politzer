60bdfe84bcab4713e6cd7c6c1c83e0c956cbc0fe
benjamin.politzer <benjamin.politzer@mymail.champlain.edu>
2023-09-04 15:40:31 -0400

    Followed Tutorial up to first NEXT STEPS

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,12 +8,41 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+unsigned int createVAO(float* vertexData, int numVertices)
+{
+	//Define a new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Tell vao to pull vertex data from vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//Define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	return vao;
+}
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
 		printf("GLFW failed to init!");
 		return 1;
 	}
+	
+	//global variable in main.cpp, or in main()
+	float vertices[9] = {
+		-0.5, -0.5, 0.0,
+		 0.5, -0.5, 0.0,
+		 0.0,  0.5, 0.0
+	};
 
 	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
 	if (window == NULL) {
@@ -27,10 +56,17 @@ int main() {
 		return 1;
 	}
 
+	//Opengl setup
+	//VAO
+	//Shader Program
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Between frames/Drawing
+
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
